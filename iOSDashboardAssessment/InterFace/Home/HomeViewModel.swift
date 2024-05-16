0ff4b3d9e8dd37c0431e9e99d7fe78afba6cb830
sooraav <purushothaman.sourav4@gmail.com>
2024-05-17 01:45:38 +0530

    Added date and Border in greeting view

--- iOSDashboardAssessment/InterFace/Home/HomeViewModel.swift
+++ iOSDashboardAssessment/InterFace/Home/HomeViewModel.swift
@@ -26,6 +26,25 @@ class HomeViewModel {
         
         self.datafetcher = dataFetcher
     }
+    
+    private func getCurrentDate() -> String {
+        let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "EEEE, MMMM d'\(daySuffix(from: Date())),' yyyy"
+            
+        return dateFormatter.string(from: Date())
+    }
+    
+    private func daySuffix(from date: Date) -> String {
+        let calendar = Calendar.current
+        let dayOfMonth = calendar.component(.day, from: date)
+        
+        switch dayOfMonth {
+        case 1, 21, 31: return "st"
+        case 2, 22: return "nd"
+        case 3, 23: return "rd"
+        default: return "th"
+        }
+    }
 }
 
 extension HomeViewModel: HomeInterface {
@@ -47,7 +66,7 @@ extension HomeViewModel: HomeInterface {
             .receive(on: DispatchQueue.main)
             .sink { [weak self] value in
                 
-                self?.greeting = GreetingsModel(name: value.first?.customerName ?? "Unknown_Name", date: "today's date")
+                self?.greeting = GreetingsModel(name: value.first?.customerName ?? "Unknown_Name", date: self?.getCurrentDate() ?? "Unknown_Date")
             }
             .store(in: &disposables)
     }
--- iOSDashboardAssessment/InterFace/ReusableViews/GreetingsView.swift
+++ iOSDashboardAssessment/InterFace/ReusableViews/GreetingsView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct GreetingsView: View {
     
+    private struct ViewTraits {
+        
+        static let sidePadding: CGFloat = 20.0
+        static let cornerRadius: CGFloat = 8.0
+    }
+    
     let model: GreetingsModel
     var body: some View {
         
         HStack {
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Hello, \(model.name)! 👋")
                     .font(.headline)
                 Text(model.date)
@@ -25,6 +31,12 @@ struct GreetingsView: View {
             Image(systemName: "person")
                 .resizable()
                 .frame(width: 50, height: 50)
+        }
+        .padding()
+        .clipShape(RoundedRectangle(cornerRadius: ViewTraits.cornerRadius))
+        .overlay {
+            RoundedRectangle(cornerRadius: ViewTraits.cornerRadius)
+                .strokeBorder(Color.black)
         }
         .padding(20)
     }
