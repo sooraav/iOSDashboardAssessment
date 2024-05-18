74fc5554dd59535908187cdf17d137bdafc214ea
sooraav <purushothaman.sourav4@gmail.com>
2024-05-18 13:36:16 +0530

    Created Basic JobStats View

--- iOSDashboardAssessment/InterFace/ReusableViews/JobStatsView.swift
+++ iOSDashboardAssessment/InterFace/ReusableViews/JobStatsView.swift
@@ -10,13 +10,43 @@ import SwiftUI
 struct StatsView: View {
     
     let model: StatsModel
-    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     var body: some View {
        
+        VStack(alignment: .leading){
+            Text(model.type == .job ? "Job Stats": "InVoiceStats")
+                .padding(.leading, 10)
+            Divider()
+            
+            LazyVGrid(columns: columns) {
+        
+                ForEach(model.barInfo, id: \.name) { barInfo in
+                    if let count = barInfo.count {
+                        let countText = model.type == .job ? " (\(count))" : " ($\(count))"
+                        HStack {
+                            barInfo.colour
+                                .frame(width: 20, height: 20)
+                                .cornerRadius(5)
+                            Text(barInfo.name + countText)
+                            
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
         
     }
 }
 
+let barInfo = [BarModel(name: "Hello", count: 10, colour: .red),
+               BarModel(name: "Hi", count: 5, colour: .blue),
+               BarModel(name: "Aloha", count: 3, colour: .green)
+]
 #Preview {
-    JobStatsView()
+    StatsView(model: StatsModel(barInfo: barInfo, type: .job))
 }
