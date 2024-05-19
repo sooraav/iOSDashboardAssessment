95566d228b8071e1d509c7472aff75466f4708bb
sooraav <purushothaman.sourav4@gmail.com>
2024-05-19 12:09:02 +0530

    Added date formats

--- iOSDashboardAssessment/InterFace/ReusableViews/JobItemView.swift
+++ iOSDashboardAssessment/InterFace/ReusableViews/JobItemView.swift
@@ -16,7 +16,38 @@ struct JobItemView: View {
         VStack(alignment: .leading) {
             Text("#\(model.jobNumber)")
             Text(model.title)
-            Text("\(model.startTime) -> \(model.endTime)")
+            Text(formatDates(startDateStr:model.startTime, endDateStr:model.endTime))
+        }
+    }
+    
+    func formatDates(startDateStr: String, endDateStr: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        guard let startDate = dateFormatter.date(from: startDateStr),
+              let endDate = dateFormatter.date(from: endDateStr) else {
+            return "Invalid date format"
+        }
+        
+        let calendar = Calendar.current
+        
+        if calendar.isDateInToday(startDate) && calendar.isDateInToday(endDate) {
+            dateFormatter.dateFormat = "HH:mm"
+            let startTime = dateFormatter.string(from: startDate)
+            let endTime = dateFormatter.string(from: endDate)
+            return "today, \(startTime) - \(endTime)"
+        } else if calendar.isDate(startDate, inSameDayAs: endDate) {
+            dateFormatter.dateFormat = "dd/MM/yy HH:mm"
+            let startDateTime = dateFormatter.string(from: startDate)
+            dateFormatter.dateFormat = "HH:mm"
+            let endTime = dateFormatter.string(from: endDate)
+            return "\(startDateTime) - \(endTime)"
+        } else {
+            dateFormatter.dateFormat = "dd/MM/yy HH:mm"
+            let startDateTime = dateFormatter.string(from: startDate)
+            let endDateTime = dateFormatter.string(from: endDate)
+            return "\(startDateTime) -> \(endDateTime)"
         }
     }
 }
